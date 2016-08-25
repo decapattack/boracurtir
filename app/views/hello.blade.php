fc742ff3808099ccbd8edbb881fc7cedf1b6ab2d
decapattack <decapattack@gmail.com>
2016-08-25 12:49:33 -0300

    Mais um passo

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,9 +1,14 @@
 @extends("layouts.default")
 
 @section("content")
-<div class="welcome">
+<div class="botao-pagina-principal">
 
-    Teste
+    <p>Onde tem cerveja por perto?</p>
+
+    <img src="img/logo.png">
+    <br>
+    Clique para saber!
 
 </div>
+<div class="background-botao-pagina-principal"></div>
 @stop
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,12 +1,33 @@
 <!doctype html>
 <html>
     <header>
-        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+        {{ HTML::style('css/materialize.min.css') }}
+        {{ HTML::style('css/main.css') }}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="js/materialize.min.js"></script>
     </header>
     <body>
-        <div class="container">
+        <!-- Dropdown Structure -->
+        <ul id="dropdown1" class="dropdown-content">
+            <li><a href="#!">one</a></li>
+            <li><a href="#!">two</a></li>
+            <li class="divider"></li>
+            <li><a href="#!">three</a></li>
+        </ul>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#!" class="brand-logo">Logo</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <!-- Dropdown Trigger -->
+                    <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown - </a></li>
+                </ul>
+            </div>
+        </nav>
+        <div align="center">
             @yield('content')
         </div>
+
     </body>
 </html>
